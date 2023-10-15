a1c49c971a158582d9e022a91587aacf7d4b0352
Tyuiu.OdarenkoAR@outlook.com <odarenkoalina@mail.ru>
2023-10-15 21:17:16 +0500

    Task2

---
+++ Tyuiu.OdarenkoAR.Sprint1.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.OdarenkoAR.Sprint1.Task2.V15.Lib;
+namespace Tyuiu.OdarenkoAR.Sprint1.Task2.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int a = 2;
+            var res = ds.CalculateCubeVolume(a);
+            Assert.AreEqual(8, res);
+        }
+    }
+}
---
+++ Tyuiu.OdarenkoAR.Sprint1.Task2.V15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.OdarenkoAR.Sprint1.Task2.V15.Lib;
+namespace Tyuiu.OdarenkoAR.Sprint1.Task2.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Одаренко А. Р. | ИСПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила Одаренко А. Р. | ИСПб-23-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные рассчёты и печатает результат на экране.            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Объём куба = " + ds.CalculateCubeVolume(x));
+            Console.ReadLine();
+        }
+    }
+}
